11cbf8d8f93dff9067f6033ab4ba750f38200560
‘hua934562127 <934562127@qq.com>
2019-05-29 19:43:40 +0800

    找房子

---
+++ js/rentalList.js
@@ -0,0 +1,8 @@
+window.onload =function () {
+    var banner = document.querySelector('.banner'),
+        ul = document.querySelector(".banner ul"),
+        lis = document.querySelectorAll(".banner li"),
+        nums = document.querySelector(".nums"),
+        Height = lis[0].offsetHeight,
+        Width = banner.offsetWidth;
+}
